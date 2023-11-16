6e54d36b0dd409de2d57aebf7ea25e9f7b9cac8d
sooraj <soorajks2002@gmail.com>
2023-11-16 19:34:15 +0530

    assistant with good console visualization

--- assistant.py
+++ assistant.py
@@ -1,58 +1,132 @@
 from openai import Client
 from api_key import key
 import time
+import sys
 
 client = Client(api_key=key)
 
-file = client.files.create(
-    file=open("attention_is_all_you_need.pdf", "rb"),
-    purpose='assistants'
-)
 
-tone = int(input("Please select the tone of the response you want : \n1. Academic\n2. Creative\n3. Aggressive\n"))
-length = int(input("Please select the response length you need : \n1. Short\n2. Medium\n3. Long\n"))
+def load_document(document_path):
+    '''Creates OpenAI file object for the document at the given file path and returns it's file.id'''
 
-tone = "Academic" if tone==1 else "Creative" if tone==2 else "Aggressive"
-length = "Short" if length == 1 else "Medium" if length == 2 else "Long"
-
-# print(tone, " and ", length)
-
-
-assistant = client.beta.assistants.create(
-    name="Research Assistant",
-    description=f"You are a research assistant. You would respond to user's queries regarding any research paper they share. Your response should be {tone} and output length should be {length}. ",
-    model="gpt-4-1106-preview",
-    tools=[{"type": "retrieval"}],
-    file_ids=[file.id]
-)
-
-thread = client.beta.threads.create()
-
-for i in range(3) :
-    
-    user_input = input("\nUser : ")
-
-    message = client.beta.threads.messages.create(
-        thread_id=thread.id,
-        role="user",
-        content=user_input,
-        # file_ids=[file.id]
+    file = client.files.create(
+        file=open(document_path, "rb"),
+        purpose='assistants'
     )
 
+    return file.id
+
+
+def load_assistant (file_id) :
+    '''Creates an AI-Assistant which will act as a Research Assistant. Assistant will have retrieval capabilities with the file whose file.id is provided'''
+
+    assistant = client.beta.assistants.create(
+        name="Research Assistant",
+        description=f"You are responsible for assisting users with their queries regarding research papers that they share. Your answer to user queries must be generated strictly based on data avialabel in the research paper.",
+        model="gpt-3.5-turbo-1106",
+        tools=[{"type": "retrieval"}],
+        file_ids=[file_id],
+    )
+        
+    return assistant.id
+
+
+def thinking_animation(animation_text):
+    '''Create animation with "animeation_text"'''
+    animation_frames = [f"{animation_text}      ", f"{animation_text} .    ", f"{animation_text} . .  ", f"{animation_text} . . ."]
+    for frame in animation_frames:
+        sys.stdout.write('\r' + frame)
+        sys.stdout.flush()
+        time.sleep(0.2)
+
+
+def extract_abstract(thread_id, assistant_id) :
+    '''Extracts the complete abstract part from the file shared with the assistant whoose assistant.id is shared'''
+    
+    message = client.beta.threads.messages.create(
+        thread_id=thread_id,
+        role="user",
+        content="Get the abstract of the paper",
+    )
+
+
     run = client.beta.threads.runs.create(
-        thread_id=thread.id,
-        assistant_id=assistant.id,
+        thread_id=thread_id,
+        assistant_id=assistant_id,
     )
 
     while run.status != "completed":
-        time.sleep(1)
+        
+        # for animation 
+        sys.stdout.write('\r' + ' ' * 20 + '\r')
+        thinking_animation("Extracing Abstract")
+        
         run = client.beta.threads.runs.retrieve(
-            thread_id=thread.id,
+            thread_id=thread_id,
             run_id=run.id
         )
 
-    messages = client.beta.threads.messages.list(thread_id=thread.id)
+    messages = client.beta.threads.messages.list(thread_id=thread_id)
 
-    print("\nAssistant : " , messages.data[0].content[0].text.value)
+    print(f"\nAbstract ::\n{messages.data[0].content[0].text.value}\n\n")
 
-client.files.delete(file.id)
+
+
+def paraphrase_abstract(thread_id, assistant_id, tone, length) :
+    '''Paraphrase the abstract from the research paper with given tone and length of response.'''
+    
+    
+    message = client.beta.threads.messages.create(
+        thread_id=thread_id,
+        role="user",
+        content=f"Paraphrase the abstract with {tone} tone and {length} the abstract from the paper in terms of length",
+    )
+
+    run = client.beta.threads.runs.create(
+        thread_id=thread_id,
+        assistant_id=assistant_id,
+    )
+
+    while run.status != "completed":
+        
+        # for animation 
+        sys.stdout.write('\r' + ' ' * 20 + '\r')
+        thinking_animation("Paraphrasing Abstract")
+        
+        run = client.beta.threads.runs.retrieve(
+            thread_id=thread_id,
+            run_id=run.id
+        )
+
+    messages = client.beta.threads.messages.list(thread_id=thread_id)
+
+    print(f"\nParaphrased Abstract ::\n{messages.data[0].content[0].text.value}\n\n") 
+
+
+
+if __name__ == "__main__":
+    
+    research_paper_path = "attention_is_all_you_need.pdf"
+    
+    file_id = load_document(research_paper_path)
+    
+    assistant_id = load_assistant(file_id)
+    
+    thread = client.beta.threads.create()
+    
+    extract_abstract(thread.id, assistant_id)
+    
+
+    tone = int(input(
+        "Please select the tone of the praphrase you want : \n1. Academic (default)\n2. Creative\n3. Aggressive\n"))
+    length = int(input(
+        "Please select the response length of paraphrase : \n1. Same length (default)\n2. Twice the length\n3. Thrice the length\n"))
+
+    tone = "Aggressive" if tone == 3 else "Creative" if tone == 2 else "Academic"
+    length = "thrice the length of " if length == 3 else "twice the length of " if length == 2 else "same length as"
+    
+    
+    paraphrase_abstract(thread.id, assistant_id, tone, length)
+
+
+    client.files.delete(file_id)
